1dc0e5d1ecb7c1814f6e92d435456edba91f6b98
Diogo Correia <me@diogotc.com>
2022-04-19 19:58:37 +0100

    Add product list page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -9,12 +9,15 @@ enum Route {
     Home,
     #[at("/hello-server")]
     HelloServer,
+    #[at("/products")]
+    ProductPage,
 }
 
 fn switch(routes: &Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
         Route::HelloServer => html! { <HelloServer /> },
+        Route::ProductPage => html! {<ProductPage />},
     }
 }
 
@@ -74,6 +77,105 @@ fn hello_server() -> Html {
             }
         }
     }
+}
+
+#[function_component(Navbar)]
+fn navbar() -> Html {
+    html! {
+        <header class="navbar">
+            <h1 class="site-title">{"Frigu"}</h1>
+            <div class="nav-links">
+                <a href="/products" class="active">{"Products"}</a>
+                <a href="/my-transactions">{"Transactions"}</a>
+            </div>
+        </header>
+    }
+}
+
+#[function_component(ProductPage)]
+fn product_page() -> Html {
+    /*let products = use_state(|| None);
+
+    {
+        let products = products.clone();
+        use_effect(move || {
+            if products.is_none() {
+                spawn_local(async move {
+                    // TODO
+                })
+            }
+        })
+    }*/
+
+    html! {
+        <>
+            <Navbar />
+            <main>
+                <div class="card products-card">
+                    <div class="card-header">
+                        {"Products"}
+                    </div>
+                    <div class="card-content">
+                        <div class="product-list">
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                            <ProductItem name="Napolitanas" description="Cenas com chocolate. Yummy!" seller="Rafael Girão" stock={20} price={50} />
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    }
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct ProductItemProps {
+    name: String,
+    description: String,
+    seller: String,
+    stock: i32,
+    price: u32,
+}
+
+#[function_component(ProductItem)]
+fn product_item(product: &ProductItemProps) -> Html {
+    html! {
+        <div class="product-item">
+            <div class="product-info">
+                <div class="product-info--name">{product.name.clone()}</div>
+                <div class="product-info--metadata">
+                    {"By "}
+                    <span class="product-info--seller">{product.seller.clone()}</span>
+                    {" | "}
+                    <span class="product-info--stock">{product.stock}</span>
+                    {" in stock"}
+                </div>
+                <div class="product-info--description">{product.description.clone()}</div>
+            </div>
+            <div class="product-price">
+                {format_display_price(product.price)}
+            </div>
+            <div class="product-actions">
+                <button class="product-actions--purchase">{"Buy"}</button>
+            </div>
+        </div>
+    }
+}
+
+fn format_display_price(price: u32) -> String {
+    format!("{}.{:02}€", price / 100, price % 100)
 }
 
 fn main() {
