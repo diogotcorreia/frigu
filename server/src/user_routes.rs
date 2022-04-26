9670cf55771015b412b50ea5cd236cb425329c5d
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2022-04-26 12:10:04 +0100

    Implement login, jwt for auth, some error handling (#5)

---
+++ server/src/user_routes.rs
@@ -0,0 +1,29 @@
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use axum::{extract, Extension};
+use axum_extra::extract::cookie::CookieJar;
+use sea_orm::{prelude::*, DatabaseConnection};
+
+use entity::user;
+
+use crate::{dtos::LoginDto, errors::AppError};
+
+pub(crate) async fn login(
+    extract::Json(login_dto): extract::Json<LoginDto>,
+    Extension(ref conn): Extension<DatabaseConnection>,
+    jar: CookieJar,
+) -> Result<CookieJar, AppError> {
+    let user = user::Entity::find_by_id(login_dto.id)
+        .one(conn)
+        .await?
+        .ok_or(AppError::NoSuchUser)?;
+
+    let password = login_dto.password;
+
+    let hashed_password = user.hashed_password;
+    let password_hash =
+        PasswordHash::new(&hashed_password).expect("saved password hash must be valid");
+    Argon2::default().verify_password(password.as_bytes(), &password_hash)?;
+
+    let user_cookie = crate::jwt_helpers::new_cookie(user.id)?;
+    Ok(jar.add(user_cookie))
+}
